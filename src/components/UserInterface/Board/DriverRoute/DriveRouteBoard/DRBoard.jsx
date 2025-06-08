4b7ad3884451881461d4f761641dc494b43b4328
jiwoolee2 <dlwldn1009@naver.com>
2025-06-08 21:21:14 +0900

    comment

--- src/components/UserInterface/Board/DriverRoute/DriveRouteBoard/DRBoard.jsx
+++ src/components/UserInterface/Board/DriverRoute/DriveRouteBoard/DRBoard.jsx
@@ -69,6 +69,7 @@ import { CustomPrev, CustomNext } from "../CustomSlides/CustomSlides";
 import SelectBoard from "./BoardCRUD/SelectBoard";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import BoardModal from "./BoardCRUD/BoardModal";
+import SelectComment from "./CommentCRUD/SelectComment";
 const DRBoard = () => {
   const [openCommentModal, setOpenCommentModal] = useState(false);
   const [openPhotoModal, setopenPhotoModal] = useState(false);
@@ -76,7 +77,6 @@ const DRBoard = () => {
   const [openMapModal, setOpenMapModal] = useState(false);
   const [openDriveRoute, setOpenDriveRoute] = useState(false);
   const [mapUrl, setMapUrl] = useState("");
-  const [heart, setHeart] = useState(true);
   const ref = useRef(null);
   const [imagesUrl, setImagesUrl] = useState([]);
   const [boardImage, setBoardImage] = useState([]);
@@ -322,6 +322,16 @@ const DRBoard = () => {
   };
 
   // ----------------------댓글 조회----------------------
+  const handleCommentList = (board) => {
+    setCommentTargetBoard({
+      memberNickName: board.memberNickName,
+      memberNo: board.memberNo,
+      boardNo: board.boardNo,
+      boardContent: board.boardContent,
+    });
+    setOpenCommentModal(true);
+  };
+
   useEffect(() => {
     if (!commentTargetBoard) return;
     axios
@@ -549,16 +559,6 @@ const DRBoard = () => {
       });
   };
 
-  const handleCommentList = (board) => {
-    setCommentTargetBoard({
-      memberNickName: board.memberNickName,
-      memberNo: board.memberNo,
-      boardNo: board.boardNo,
-      boardContent: board.boardContent,
-    });
-    setOpenCommentModal(true);
-  };
-
   const handleDriveRoute = (e) => {
     setOpenDriveRoute(true);
     setSrcMap(e.driveRouteImage);
@@ -691,141 +691,23 @@ const DRBoard = () => {
             ref={ref}
           />
 
-          {/* 댓글 모달 */}
-          {openCommentModal && (
-            <CommentModalWrapper>
-              <CloseBtn onClick={() => setOpenCommentModal(false)}>
-                <CloseRoundedIcon style={{ fontSize: "40px" }} />
-              </CloseBtn>
-              <CommentModalLabel>
-                <ModalHeader>상세보기</ModalHeader>
-                <ModalContent>
-                  <LeftComment>
-                    <BoardImage>
-                      <Slider {...settings}>
-                        {boardImages
-                          .filter(
-                            (item) =>
-                              item.boardNo === commentTargetBoard.boardNo
-                          )
-                          .map((item, index) => {
-                            return (
-                              <div key={index}>
-                                <img
-                                  src={item.boardImage}
-                                  style={{
-                                    width: "100%",
-                                    height: "400px",
-                                    objectFit: "cover",
-                                    borderRadius: "8px",
-                                  }}
-                                  alt={`preview-${index}`}
-                                />
-                              </div>
-                            );
-                          })}
-                      </Slider>
-                    </BoardImage>
-
-                    <BoardContent>
-                      {commentTargetBoard.boardContent}
-                    </BoardContent>
-                  </LeftComment>
-                  <RightContent>
-                    <Comments>
-                      {commentInfo.map((comment, commentNo) => (
-                        <CommentItem key={commentNo}>
-                          <CommentTop>
-                            <CommentAuthor>
-                              {comment.memberNickname}
-                            </CommentAuthor>
-                            {comment.commentWriter == auth.user.memberNo && (
-                              <CommentButtonGroup>
-                                {editingCommentNo == comment.commentNo ? (
-                                  <>
-                                    <span
-                                      className="save"
-                                      onClick={() =>
-                                        handleUpdateComment(comment.commentNo)
-                                      }
-                                    >
-                                      저장
-                                    </span>
-                                    <span
-                                      className="cancel"
-                                      onClick={() => setEditingCommentNo(null)}
-                                    >
-                                      취소
-                                    </span>
-                                  </>
-                                ) : (
-                                  <>
-                                    <span
-                                      className="edit"
-                                      onClick={() => {
-                                        setEditingCommentNo(comment.commentNo);
-                                        setEditedContent(
-                                          comment.commentContent
-                                        ); // 현재 내용 가져오기
-                                      }}
-                                    >
-                                      수정
-                                    </span>
-                                    <span
-                                      className="delete"
-                                      onClick={() =>
-                                        handleDeleteComment(comment.commentNo)
-                                      }
-                                    >
-                                      삭제
-                                    </span>
-                                  </>
-                                )}
-                              </CommentButtonGroup>
-                            )}
-                          </CommentTop>
-
-                          {editingCommentNo === comment.commentNo ? (
-                            <input
-                              type="text"
-                              value={editedContent}
-                              onChange={(e) => setEditedContent(e.target.value)}
-                              maxLength={85}
-                              style={{ width: "100%", marginTop: "5px" }}
-                            />
-                          ) : (
-                            <CommentText>{comment.commentContent}</CommentText>
-                          )}
-                        </CommentItem>
-                      ))}
-                    </Comments>
-                    {hasMoreComment && (
-                      <CommentSubmit onClick={handleMoreComments}>
-                        댓글 더보기
-                      </CommentSubmit>
-                    )}
-                    <InsertComment>
-                      <Commentarea
-                        type="text"
-                        placeholder="댓글 달기.."
-                        maxLength={85}
-                        value={comment.commentContent} // 추가
-                        onChange={(e) =>
-                          setComment({
-                            ...comment,
-                            commentContent: e.target.value,
-                          })
-                        }
-                      />
-                      <CommentSubmit onClick={handleComment}>
-                        게시
-                      </CommentSubmit>
-                    </InsertComment>
-                  </RightContent>
-                </ModalContent>
-              </CommentModalLabel>
-            </CommentModalWrapper>
-          )}
+          <SelectComment
+            openCommentModal={openCommentModal}
+            commentTargetBoard={commentTargetBoard}
+            commentInfo={commentInfo}
+            editingCommentNo={editingCommentNo}
+            hasMoreComment={hasMoreComment}
+            handleMoreComments={handleMoreComments}
+            handleComment={handleComment}
+            comment={comment}
+            boardImages={boardImage} // 게시물 이미지 정보 전달
+            setOpenCommentModal={setOpenCommentModal}
+            setEditingCommentNo={setEditingCommentNo}
+            setEditedContent={setEditedContent}
+            editedContent={editedContent}
+            handleDeleteComment={handleDeleteComment}
+            handleUpdateComment={handleUpdateComment}
+          />
         </RentBodyDiv>
       </RentContainerDiv>
     </>
